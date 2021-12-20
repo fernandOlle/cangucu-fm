1124deb8ea9c96c5198d116d3f609a5dd848499f
frolle <frolle@inf.ufpel.edu.br>
2021-12-20 11:19:25 -0300

    remove autoplay

--- components/player.jsx
+++ components/player.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
-const Player = () => {
+const Player = ({ playing }) => {
   return (
     <div className='drop-shadow-2xl'>
-      <audio
-        className='drop-shadow-2xl rounded-lg p-0 mb-8 w-full'
-        controls
-        autoPlay
-      >
+      <audio className='drop-shadow-2xl rounded-lg p-0 mb-8 w-full' controls>
         <source src='http://r15.ciclano.io:4445/stream' type='audio/mpeg' />
       </audio>
     </div>
