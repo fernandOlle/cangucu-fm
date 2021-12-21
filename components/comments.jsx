f32163a9af8eb9f9d55d19be1d30a467a753a72f
frolle <frolle@inf.ufpel.edu.br>
2021-12-21 05:19:21 -0300

    add comment function

--- components/comments.jsx
+++ components/comments.jsx
@@ -18,13 +18,14 @@ const Comments = ({ slug }) => {
       {comments.length > 0 && (
         <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
           <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
-            {comments.length} Comments
+            {comments.length}{' '}
+            {comments.length > 1 ? 'Comentários' : 'Comentário'}
           </h3>
           {comments.map((comment, index) => (
             <div key={index} className='border-b border-gray-100 mb-4 pb-4'>
               <p className='mb-4'>
                 <span className='font-semibold'>{comment.name}</span> on{' '}
-                {moment(comment.createdAt).format('MMM DD, YYYY')}
+                {moment(comment.createdAt).format('DD/MM/YYYY')}
               </p>
               <p className='whitespace-pre-line text-gray-600 w-full'>
                 {parse(comment.comment)}
