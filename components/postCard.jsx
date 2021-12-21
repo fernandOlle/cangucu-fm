a84b6aeaa866c20dac28fcb815d8933a7961eab3
frolle <frolle@inf.ufpel.edu.br>
2021-12-20 23:39:10 -0300

    post card overhall

--- components/postCard.jsx
+++ components/postCard.jsx
@@ -7,7 +7,7 @@ import { Image } from 'next/image';
 
 const PostCard = ({ post }) => {
   return (
-    <div className='bg-white cardPop shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
+    <div className='bg-white  shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
       <div className='relative overflow-hidden shadow-md pb-80 mb-6 '>
         <img
           src={post.featuredImage.url}
@@ -57,7 +57,7 @@ const PostCard = ({ post }) => {
       </p>
       <div className='text-center'>
         <Link href={`/post/${post.slug}`}>
-          <span className='transition duration-500 ease transform hover:-translate-y-1 inline-block bg-orange-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer'>
+          <span className='transition duration-500 ease-in-out transform hover:-translate-y-3 hover:font-semibold inline-block bg-red-700 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer'>
             Continue Lendo
           </span>
         </Link>
