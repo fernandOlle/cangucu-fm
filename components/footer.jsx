df707642315d6d7a6334e3a7478d20019ec0c925
Fernando Ollé <55040050+fernandOlle@users.noreply.github.com>
2022-01-25 02:10:02 -0300

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -28,11 +28,11 @@ const Footer = () => {
           <span className=' font-normal text-xl'>
             WhatsApp: +55 (53) 98415-2811 <br />
             Fixo: +55 (53) 3252-2811 <br />
-            Email: contato@ canguçufm.com.br
+            Email: contato@ canguçufm .com.br
           </span>
         </span>
         <span className='font-semibold text-xl  flex-1 content-end'>
-          @Redes So
+          @Redes Sociais
           <br />
           <br />
           {medias
